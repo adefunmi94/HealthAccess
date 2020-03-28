55d63b21933fb698095b41e38dc415f49c8020c5
oyeyipoadefunmi90@gamil.com <oyeyipoadefunmi90@gamil.com>
2020-03-28 18:07:26 +0100

    fisrt commit on ussd

---
+++ src/test/java/com/healthaccessdemo/healthdemo/HealthdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthaccessdemo.healthdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
