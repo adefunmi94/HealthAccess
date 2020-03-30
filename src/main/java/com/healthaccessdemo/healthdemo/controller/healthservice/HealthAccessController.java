7d278d1c59ebe44e1451080f7ed92d72c6157f4e
oyeyipoadefunmi90@gamil.com <oyeyipoadefunmi90@gamil.com>
2020-03-30 12:30:47 +0100

    In progress

--- src/main/java/com/healthaccessdemo/healthdemo/controller/healthservice/HealthAccessController.java
+++ src/main/java/com/healthaccessdemo/healthdemo/controller/healthservice/HealthAccessController.java
@@ -57,19 +57,42 @@ public class HealthAccessController {
 		
 		 case "1": 
 			 
-	            return "CON Connecting you to General Doctor ->>>\n Please Hold on\n"
-	            		+ "0. go to Menu"+ res;
+	            return "CON Connecting you to General Doctor ->>>\n Please Hold on";
+	            		
 	            
 		 	case "2":
 			 
 			 return "CON Connecting-->>>> Pediatrician\n"
-			 		+ "0. quit" + res;
+			 		+ "0. quit";
 			 
-		case "7":
-			 
-			 return "End QUit Service";
+		 	case "3":
+				 
+				 return "CON Connecting-->>>> Gynecologist\n"
+				 		+ "0. quit";
+				 
+		 	case "4":
+				 
+				 return "CON Connecting-->>>>Dentistian\n"
+				 		+ "0. quit";
+				 
+				 
+		 	case "5":
+				 
+				 return "CON Connecting-->>>>Dentistian\n"
+				 		+ "0. quit";
+				 
+		 	case "6":
+				 
+				 return "CON Connecting-->>>>Dentistian\n"
+				 		+ "0. quit";
+				 
+				 
+				 
+				 case "7":
+					 
+					 return "End QUit Service";
+		
 			 		
-			 
 		default:   
 	            return "Thank you"; 
 		}
