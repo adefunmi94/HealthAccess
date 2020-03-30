6a1dbc98f16bbad63e2bd2a8eab4606b3eae5a3f
oyeyipoadefunmi90@gamil.com <oyeyipoadefunmi90@gamil.com>
2020-03-30 06:26:48 +0100

    In progress

--- src/main/java/com/healthaccessdemo/healthdemo/controller/healthservice/HealthAccessController.java
+++ src/main/java/com/healthaccessdemo/healthdemo/controller/healthservice/HealthAccessController.java
@@ -15,63 +15,51 @@ import com.healthaccessdemo.healthdemo.service.HealthAccessService;
 
 @RestController
 public class HealthAccessController {
+	
 	Logger logger = Logger.getLogger(getClass().getName());
-	 
+ 
 //	private String sessionId;
 	private String response;
 	
 	private HealthAccessService theHealthPro;
-	private String message = " ";
+
 	
 	
 	@PostMapping("/")
 	public String showUssd(@RequestParam String text, HttpServletResponse response) {
 		 
-		response.addHeader("Content-type: text/plain", "Value-HttpServletResponse");
+		
+		 response.addHeader("Content-type: text/plain", "Value-HttpServletResponse");
 		 
 		 String res;
 		 
+		 logger.info(""+text);
 		   
 		if(text.equals("")) {
 		
 		 logger.info("empty");
 
-			 return "CON Welcome to Old Mutual MumTribe,\n Which of our service do need? \n"
-//			 		+ "1. Register\n"
-			 		+ "1. Menu\n";
-//			 		+ "3. About Us\n";
-			 		
+			 return "CON Welcome to Old Mutual Nigeria,\n Which of the Health Proffesional do you need? \n"
+			 		+ "1. General Doctor\n"
+			 		+ "2. Pediatrician\n"
+			 		+ "3. Gynecologist\n"
+			 		+ "4. Dentistian\n"
+			 		+ "5. Optician\n"
+			 		+ "6. Neurologist \n"
+			 		+ "7. Quit Service";
+			 
 //			 logger.info("waooo! keep it up");
 			 
-		}		
+		}
 		
 		else if(text.equals("1")) {
 			
 //			 logger.info("option 1");
-				      return "CON Welcome to MumTribe 24/7 access to Health professionals\n Select your Doctor: \n"
-				        + "1.  General Doctor\n"
-				 		+ "2. Pediatrician\n"
-				 		+ "3. Gynecologist\n"
-				 		+ "4. Dentistian\n"
-				 		+ "5. Optician\n"
-				 		+ "6. Neurologist \n"
-				 		+ "7. Quit Service";
-				 
-		}    
-				      else if(text.equals("1")) {
-							
-//							 logger.info("option 1");
-								      return  " CON Please hold while Connecting you to General Doctor ->>>>\n"
-								    	  		+ "0. to quit";
-						
-								      
-//								      if(text.equals("1")) {
-//								    	  
-//								    	  return "Please hold while Connecting you to General Doctor ->>>>\n"
-//								    	  		+ "0. to quit";
-//								      }
-						
-				      
+//			 
+			
+				      return "CON You have selected Option 1\n"
+				      		+ "1. call the available Doctor\n"
+				      		+ "0. to quit";
 //			response =  "END Quit service";
 ////			logger.info("it working right");	
 ////
@@ -82,22 +70,21 @@ public class HealthAccessController {
 //////			
 //////			response = theHealthPro.addProffessionalRespose(option).concat("Quit service");
 ////		}
-								      
 		}
-		else if(text.equals("2")) {
+		else if(text.equals("3")) {
 			
 			 logger.info("option 3");
 			 
 			return "CON You have selected Pediatrician\n"
 					+ "1. call the available Doctor\n"
-					+ "0. to go back";
+					+ "0 to go back";
 					
 		}
-		else if(text.equals("3")) {
+		else if(text.equals("2")) {
 			
-			 logger.info("option 4");
+			 logger.info("option 2");
 			 
-			return "CON Please hold while Connecting you to General Doctor ->>>>";
+			return "CON You have selected Gynecologist";
 					
 		}
 		else if(text == "0") {
@@ -117,15 +104,13 @@ public class HealthAccessController {
 //	.
 	
 //		return res;
-
-		 	}
+	}
 	
 	
 //	@GetMapping("/")
 //	public String showUssd(@RequestParam String text) {
 //		
 //		return "";
-	
+//	}
 
-
-	}
+}
